29010b5a6a1e1015575f70e0dc75a0748b8667e8
Tomáš Peroutka <67205912+Pero-coder@users.noreply.github.com>
2023-11-18 00:11:20 +0100

    Add MongoDB integration and API endpoint for adding lecturers Create new lecturer #2 Get all lecturers #3

--- app/app.py
+++ app/app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template
+import os
+import json
+from bson import json_util
+from dotenv import load_dotenv
+from flask import Flask, render_template, request
+from pymongo.mongo_client import MongoClient
 
+
+load_dotenv()
+client = MongoClient(
+    f'mongodb+srv://pero:{os.environ.get("MONGO_PWD")}@cluster0.ebiunpa.mongodb.net/?retryWrites=true&w=majority'
+)
 app = Flask(__name__)
+db = client.test_database
+lecturers = db.lecturers
+
 
 @app.route("/")
 def hello_world():
@@ -13,6 +26,15 @@ def api():
 @app.route("/lecturer")
 def lecturer():
     return render_template("lecturer.html")
+
+@app.route("/api/lecturers", methods=["GET", "POST"])
+def api_lecturers():
+    if request.method == 'POST':
+        new_lecturer = request.get_json()
+        print(new_lecturer)
+        if new_lecturer:
+            lecturers.insert_one(new_lecturer)
+    return json.loads(json_util.dumps({"lecturers": list(lecturers.find())}))
 
 
 if __name__ == '__main__':
