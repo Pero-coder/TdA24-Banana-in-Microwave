a0bb558e02ffbf489b865ef6c2c80560c842813f
RxiPland <liborvanura@gmail.com>
2023-11-18 10:52:50 +0100

    renamed id variable to uuid

--- app/app.py
+++ app/app.py
@@ -36,10 +36,10 @@ def api_lecturers():
             lecturers.insert_one(new_lecturer)
     return json.loads(json_util.dumps({"lecturers": list(lecturers.find())}))
 
-@app.route("/api/delete/<string:id>", methods=["DELETE"])
-def api_delete(id):
-    deleted = bool(lecturers.delete_one({"_id": ObjectId(id)}).deleted_count)
-    
+@app.route("/api/delete/<string:uuid>", methods=["DELETE"])
+def api_delete(uuid):
+    deleted = bool(lecturers.delete_one({"_id": ObjectId(uuid)}).deleted_count)
+
     if deleted:
         return '', 204
     else:
