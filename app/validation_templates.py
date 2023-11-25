77283bc71319c40071825501127f4f4bb7d80be8
RxiPland <liborvanura@gmail.com>
2023-11-25 21:52:10 +0100

    Created Lecturer data validation model #14

---
+++ app/validation_templates.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field
+from typing import Optional, List, Dict, Literal
+
+
+
+class Lecturer(BaseModel):
+    uuid: str = Field(..., extra={'readOnly': True})
+    title_before: Optional[str] = None
+    first_name: str
+    middle_name: Optional[str] = None
+    last_name: str
+    title_after: Optional[str] = None
+    picture_url: Optional[str] = None
+    location: Optional[str] = None
+    claim: Optional[str] = None
+    bio: Optional[str] = None
+    tags: List[Dict[Literal["uuid", "name"], str]] = Field(..., min_items=0, uniqueItems=True)
+    price_per_hour: Optional[int] = Field(None, ge=0)
+    contact: Dict[Literal["telephone_numbers", "emails"], List[str]] = Field(..., min_items=1, uniqueItems=True)
