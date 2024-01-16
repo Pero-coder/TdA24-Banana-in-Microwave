ead0a97cddb8095f3a434b76cbab17fed4e44913
RxiPland <liborvanura@gmail.com>
2024-01-16 19:26:06 +0100

    redirect on homepage if invalid URL

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,18 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 from app import app, api
 
 
 @app.route("/lecturer/<string:uuid>")
 def lecturer(uuid: str):
-    lecturer = api.get_specific_lecturer(uuid)
+    lecturer = api.get_specific_lecturer(uuid.strip())
     if lecturer[1] == 404:
         return lecturer
 
     return render_template("lecturer.html", **lecturer[0])
 
+@app.route("/lecturer")
+def lecturer_empty():
+    return redirect('/')
 
 @app.route("/")
 def hello_world():
